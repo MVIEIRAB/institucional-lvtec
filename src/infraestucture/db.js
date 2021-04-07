ca8df59dc622438106e3f36d813829f915e299de
Matheus Vieira <mvieirinhaa@gmail.com>
2021-04-07 20:20:19 -0300

    teste

--- src/infraestucture/db.js
+++ src/infraestucture/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/projects', {
+mongoose.connect('mongodb+srv://herokuuser:<12345>@cluster0.te9ly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
